d881b2a7109ac1bf4e640509049be2e02af8bc59
semihbozkurt <semihbozkurtsinop@gmail.com>
2026-04-01 12:08:40 +0200

    hamburg

---
+++ hamburg.js
@@ -0,0 +1,8 @@
+            const hamburger = document.getElementById('hamburger');
+            const navMenu = document.getElementById('nav-menu');
+        
+            hamburger.addEventListener('click', () => {
+            // Tıklandığında 'active' sınıfını ekle veya çıkar (toggle)
+            hamburger.classList.toggle('active');
+            navMenu.classList.toggle('active');
+            });
